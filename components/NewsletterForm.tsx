23d1f1033435f01d92b0203a689a3b47d1e67de0
Andre Vieira <andreluizhillerv@gmail.com>
2024-05-29 21:43:58 -0300

    feat: integrates newsletter

--- components/NewsletterForm.tsx
+++ components/NewsletterForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useRef, useState } from 'react'
+import React, { useCallback } from 'react'
 import { useTranslation } from 'app/[locale]/i18n/client'
 import { useParams } from 'next/navigation'
 import { LocaleTypes } from 'app/[locale]/i18n/settings'
@@ -10,51 +10,26 @@ export interface NewsletterFormProps {
   apiUrl?: string
 }
 
-const NewsletterForm = ({ apiUrl = '/api/newsletter' }: NewsletterFormProps) => {
+const NewsletterForm = ({ apiUrl }: NewsletterFormProps) => {
   const locale = useParams()?.locale as LocaleTypes
   const { t } = useTranslation(locale, 'newsletter')
-  const inputEl = useRef<HTMLInputElement>(null)
-  const [error, setError] = useState(false)
-  const [message, setMessage] = useState('')
-  const [subscribed, setSubscribed] = useState(false)
 
-  const subscribe = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
-    const emailValue = inputEl.current?.value
-    if (!emailValue) {
-      setError(true)
-      setMessage(t('messageError'))
-      return
-    }
-
-    const res = await fetch(apiUrl, {
-      body: JSON.stringify({
-        email: emailValue,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-    })
-
-    const { error } = await res.json()
-    if (error) {
-      setError(true)
-      setMessage(t('messageError'))
-    } else {
-      inputEl.current!.value = ''
-      setError(false)
-      setSubscribed(true)
-    }
-  }
+  const handleSubscribe = useCallback(async (e: React.FormEvent<HTMLFormElement>) => {
+    window.open('https://buttondown.email/AndrelhVieira', 'popupwindow')
+  }, [])
 
   return (
     <div>
       <div className="pb-1 text-lg font-semibold text-gray-800 dark:text-gray-100">
         {t('title')}
       </div>
-      <form className="flex flex-col sm:flex-row" onSubmit={subscribe}>
+      <form
+        action="https://buttondown.email/api/emails/embed-subscribe/AndrelhVieira"
+        method="post"
+        target="popupwindow"
+        onSubmit={handleSubscribe}
+        className="flex flex-col sm:flex-row"
+      >
         <div>
           <label htmlFor="email-input">
             <span className="sr-only">{t('mail')}</span>
@@ -63,29 +38,23 @@ const NewsletterForm = ({ apiUrl = '/api/newsletter' }: NewsletterFormProps) => 
               className="w-72 rounded-md px-4 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-primary-600 dark:bg-black"
               id="email-input"
               name="email"
-              placeholder={`${subscribed ? t('placeholderSuccess') : t('placeholderDefault')}`}
-              ref={inputEl}
+              placeholder={t('placeholderDefault')}
               required
               type="email"
-              disabled={subscribed}
             />
           </label>
         </div>
         <div className="mt-2 flex w-full rounded-md shadow-sm sm:ml-3 sm:mt-0">
           <button
-            className={`w-full rounded-md bg-primary-500 px-4 py-2 font-medium text-white sm:py-0 ${
-              subscribed ? 'cursor-default' : 'hover:bg-primary-700 dark:hover:bg-primary-400'
-            } focus:outline-none focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 dark:ring-offset-black`}
+            className={
+              'w-full cursor-pointer rounded-md bg-primary-500 px-4 py-2 font-medium text-white transition hover:bg-primary-700 focus:ring-2 focus:ring-primary-600 focus:ring-offset-2 dark:ring-offset-black dark:hover:bg-primary-400 sm:py-0'
+            }
             type="submit"
-            disabled={subscribed}
           >
-            {subscribed ? t('buttonSuccess') : t('buttonDefault')}
+            {t('buttonDefault')}
           </button>
         </div>
       </form>
-      {error && (
-        <div className="w-72 pt-2 text-sm text-red-500 dark:text-red-400 sm:w-96">{message}</div>
-      )}
     </div>
   )
 }
