95bbcde2d07a7999ca1e33c0676116fcfdc0359c
Andre Vieira <andreluizhillerv@gmail.com>
2024-06-26 20:16:08 -0300

    revert: implementation of recaptcha

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -15,7 +15,6 @@ import { dir } from 'i18next'
 import { LocaleTypes, locales } from './i18n/settings'
 import TwSizeIndicator from '@/components/helper/TwSizeIndicator'
 import GoogleAnalytics from '@/components/GoogleAnalytics'
-import Script from 'next/script'
 
 export async function generateStaticParams() {
   return locales.map((locale) => ({ locale }))
@@ -107,9 +106,6 @@ export default function RootLayout({
             </div>
           </SectionContainer>
         </ThemeProvider>
-        <Script
-          src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY}`}
-        />
       </body>
     </html>
   )
--- app/types/grecaptcha.d.ts
+++
@@ -1,6 +0,0 @@
-declare interface Grecaptcha {
-  ready: (callback: () => void) => void
-  execute: (secret: string, config: any) => Promise<string>
-}
-
-declare const grecaptcha: Grecaptcha
